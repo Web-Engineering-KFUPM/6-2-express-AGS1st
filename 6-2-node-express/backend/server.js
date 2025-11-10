fa6a54d95bf958e694fc9b29e69a9db119393899
AGS1st <alsamman1x2003@gmail.comm>
2025-11-10 19:05:56 +0300

    server.js

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -3,18 +3,23 @@ import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
 const app = express();
+const port = 3000;
 
 {/*write code for cors*/}
 app.use(cors());
 
 {/*write code to define routes*/}
-app.get("/quote", (req, res) => {
+app.get("/", (req, res) => {
     const quote = getRandomQuote();
-    res.json({quote});
+    res.send("Welcome to the Quote Generator API");
 });
 
+app,get("/api/quote", (req, res) => {
+    const q = getRandomQuote();
+    res.json({quote: q});
+})
 
 {/*write code to create server*/}
-app.listen(3000, () => {
-    console.log("running");
+app.listen(port, () => {
+    console.log("Server running on http://localhost:${PORT}");
 });
